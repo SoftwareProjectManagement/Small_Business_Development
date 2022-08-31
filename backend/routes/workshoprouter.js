2f5f1b8fc68b756658edfddfc24aa762647f0a70
Ishan Nadeera <86223874+IshanNadeera@users.noreply.github.com>
2022-09-01 00:20:31 +0530

    Delete Workshop Router

--- backend/routes/workshoprouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { addWorkshop } = require('../controllers/workshopcontroller.js')
-
-//Add New Worksshop
-router.post('/add', addWorkshop);
-
-module.exports = router;
