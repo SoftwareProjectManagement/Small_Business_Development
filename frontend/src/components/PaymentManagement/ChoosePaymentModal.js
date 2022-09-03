2e49c36e6308c6c51c6c1b8794cdcf35a26f2c18
Ishan Nadeera <shanf753@gmail.com>
2022-09-03 10:20:06 +0530

    Updated Choose Payment Method View

--- frontend/src/components/PaymentManagement/ChoosePaymentModal.js
+++ frontend/src/components/PaymentManagement/ChoosePaymentModal.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
 import "./ChoosePaymentModal.css";
 
 function ChoosePaymentModal({ setOpenModal }) {
+
+  function cardPayView() {
+    alert('Card Pay');
+    setOpenModal(false);
+  }
+
+  function mobilePayView() {
+    alert('Mobile Pay');
+    setOpenModal(false);
+  }
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -24,14 +34,14 @@ function ChoosePaymentModal({ setOpenModal }) {
         <div className="body">
             <div className="row">
                 <div className="column">
-                    <div className="card">
-                        <img src="/images/visa.png"></img>
-                        <p>VISA</p>
+                    <div className="card" onClick={cardPayView}>
+                        <img src="/images/card.png"></img>
+                        <p>Card Pay</p>
                     </div>
                 </div>
 
                 <div className="column">
-                    <div className="card">
+                    <div className="card" onClick={mobilePayView}>
                         <img src="/images/mobilePay.png"></img>
                         <p>Mobile Pay</p>
                     </div>
