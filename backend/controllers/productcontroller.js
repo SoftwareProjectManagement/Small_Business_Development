69184df382eae3d59aef907b996fbd01598ed94e
Ishan Nadeera <shanf753@gmail.com>
2022-09-04 12:26:55 +0530

    Created Product Controller to Add & View Single Product

---
+++ backend/controllers/productcontroller.js
@@ -0,0 +1,35 @@
+const Product = require("../models/Product");
+
+//Add a new Product to the DataBase
+exports.addProduct = async (req, res) => {
+
+    //Define Constant Variables for the attributes
+    const {name, category, price, description, imgUrl} = req.body;
+
+    //Create New Object
+    const newProduct = new Product({
+        //Initializing the Properties of Object
+        name,
+        category,
+        price,
+        description,
+        imgUrl
+    });
+
+    //Save the Object to DataBase
+    newProduct.save().then(() => {
+        res.status(200).json({ status : "New Product Added Successfully"});
+    }).catch((error) => {
+        res.status(500).json({message:"Failed to Add the Product", error:error.message})
+    })
+}
+
+exports.viewOneProduct = async (req, res) => {
+    let productId = req.params.id;
+
+    await Product.findById(productId).then((product) => {
+        res.status(200).json({ status : "Product Fetched Successfully"});
+    }).catch((error) => {
+        res.status(500).json({ status : "Error with Fetching the Product", error: error.message});
+    });
+}
