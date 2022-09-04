70c5ba77289143b9a63b6f40a422cea7201eda18
DimagiPerera <87898809+DimagiPerera@users.noreply.github.com>
2022-09-04 13:01:44 +0530

    Completed Request Management Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import AdminLogin from "./components/AdminManagement/AdminLogin";
+import AddSeller from "./components/SellerManagement/AddSeller";
 import Login from "./components/UserManagement/SignIn/SignIn";
 import SignUp from "./components/UserManagement/SignUp/SignUp";
 
@@ -8,7 +9,7 @@ function App() {
     return (
         <Router>
             <Routes>
-
+            <Route path="/request/add" element={<AddSeller/>}/>
                 <Route path="/user/signin" element={<Login/>}/>
                 <Route path="/user/signup" element={<SignUp/>} />
                 <Route path="/admin/signin" element={<AdminLogin/>} />
