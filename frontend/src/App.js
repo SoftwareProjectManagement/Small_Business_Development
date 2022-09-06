6541136adb613d769e4b585753b48a56d5f293dc
DimagiPerera <87898809+DimagiPerera@users.noreply.github.com>
2022-09-06 22:17:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import AdminLogin from "./components/AdminManagement/AdminLogin";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import AddSeller from "./components/SellerManagement/AddSeller";
-
 import Login from "./components/UserManagement/SignIn/SignIn";
 import SignUp from "./components/UserManagement/SignUp/SignUp";
 
-
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 function App() {
     return (
         <Router>
-            <Header />
             <Routes>
                 <Route path="/request/add" element={<AddSeller />} />
                 <Route path="/user/signin" element={<Login />} />
-
-
-                <Route path="/user/signup" element={<SignUp />} />
+<Route path="/user/signup" element={<SignUp />} />
                 <Route path="/admin/signin" element={<AdminLogin />} />
+                
             </Routes>
-            <Footer />
         </Router>
     );
 }
