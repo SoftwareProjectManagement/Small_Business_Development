602e38d24c04e67ca93072e873fe5d31e564cbfd
DimagiPerera <87898809+DimagiPerera@users.noreply.github.com>
2022-09-06 22:11:14 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import AdminLogin from "./components/AdminManagement/AdminLogin";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import AddSeller from "./components/SellerManagement/AddSeller";
+
 import Login from "./components/UserManagement/SignIn/SignIn";
 import SignUp from "./components/UserManagement/SignUp/SignUp";
 
@@ -12,12 +13,12 @@ function App() {
         <Router>
             <Header />
             <Routes>
-                
                 <Route path="/request/add" element={<AddSeller />} />
                 <Route path="/user/signin" element={<Login />} />
+
+
                 <Route path="/user/signup" element={<SignUp />} />
                 <Route path="/admin/signin" element={<AdminLogin />} />
-           
             </Routes>
             <Footer />
         </Router>
