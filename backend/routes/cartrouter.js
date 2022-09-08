7d43cdf063511b82fe5780d1896c47c05561f5d7
Ishan Nadeera <shanf753@gmail.com>
2022-09-08 19:55:55 +0530

    Updated Cart Router to Delete Item

--- backend/routes/cartrouter.js
+++ backend/routes/cartrouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {additem, viewCart, updateitem} = require('../controllers/cartcontroller.js')
+const {additem, viewCart, updateitem, deleteitem} = require('../controllers/cartcontroller.js')
 const userauth = require('../middleware/userauth');
 
 router.post('/add',userauth, additem);
@@ -7,5 +7,7 @@ router.post('/add',userauth, additem);
 router.get('/:id', viewCart);
 
 router.put('/update/:id', userauth, updateitem);
+
+router.delete('/delete/:id',userauth, deleteitem);
 
 module.exports = router;
