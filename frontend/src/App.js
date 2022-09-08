8177760fb6ae1d712c21f790854128eef53a7729
Hirosh Koshila <hiroshkoshila7@gmail.com>
2022-09-08 22:18:16 +0530

    loan view page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Cart from "./components/CartManagement/Cart";
 import RequestForm1 from "./components/LoanManagement/RequestForm1/RequestForm1";
-
+import ViewLoans from "./components/LoanManagement/ViewLoans/ViewLoans"
 
 function App() {
     return (
@@ -19,7 +19,8 @@ function App() {
                 <Route path="/user/signup" element={<SignUp />} />
                 <Route path="/admin/signin" element={<AdminLogin />} />
                 <Route path="/cart" element={<Cart/>} />
-                <Route path="/req_form_1" element={<RequestForm1 />} />
+                <Route path="/loan/form_1" element={<RequestForm1 />} />
+                <Route path="/loan/view_loan" element={<ViewLoans />} />
             </Routes>
             <Footer/>
         </Router>
