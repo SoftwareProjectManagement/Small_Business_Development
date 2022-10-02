fa8ae2ff6077d388f21eb0d874d4f92e566478c5
Pulasthi Tharaka <pulasthith99@gmail.com>
2022-10-02 08:29:26 +0530

    update single item page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import AdminLogin from "./components/AdminManagement/AdminLogin";
 import AddSeller from "./components/SellerManagement/AddSeller";
 import Login from "./components/UserManagement/SignIn/SignIn";
@@ -9,7 +14,7 @@ import Cart from "./components/CartManagement/Cart";
 import SingleItem from "./components/ProductManagement/SingleItem/SingleItem";
 import AddCategory from "./components/CategoryManagement/AddCategory/AddCategory";
 import ViewCategory from "./components/CategoryManagement/ViewCategory/ViewCategory";
-import ViewProdcuts from "./components/ProductManagement/ViewAllProdcuts/ViewProducts"
+import ViewProdcuts from "./components/ProductManagement/ViewAllProdcuts/ViewProducts";
 import RequestForm1 from "./components/LoanManagement/RequestForm1/RequestForm1";
 import ViewLoans from "./components/LoanManagement/ViewLoans/VIewLoans";
 import Loan from "./components/LoanManagement/LoanInterface/Loan";
@@ -19,51 +24,47 @@ import DocumentSubmission from "./components/LoanManagement/DocumentSubmission/D
 import AddProducts from "./components/ProductManagement/AddProducts/AddProducts";
 import ViewSeller from "./components/SellerManagement/ViewSeller";
 import ViewUsers from "./components/UserManagement/AllUsers/AllUsers";
-import AddProducts from "./components/ProductManagement/AddProducts/AddProducts"
 
 function App() {
-    return (
-        <Router>
-            <Header/>
-            <Routes>
-                <Route path="/request/add" element={<AddSeller />} />
-                <Route path="/user/signin" element={<Login />} />
-                <Route path="/user/signup" element={<SignUp />} />
-                <Route path="/admin/signin" element={<AdminLogin />} />
- <Route path="/cart/:id" element={<Cart/>} />
-                <Route path="/product/item/:id" element={<SingleItem/>}/>
-                <Route path="/loan/form1" element={<RequestForm1 />} />
-                <Route path="/loan/view_loan" element={<ViewLoans />} />
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/request/add" element={<AddSeller />} />
+        <Route path="/user/signin" element={<Login />} />
+        <Route path="/user/signup" element={<SignUp />} />
+        <Route path="/admin/signin" element={<AdminLogin />} />
+        <Route path="/cart/:id" element={<Cart />} />
+        <Route path="/product/item/:id" element={<SingleItem />} />
+        <Route path="/loan/form1" element={<RequestForm1 />} />
+        <Route path="/loan/view_loan" element={<ViewLoans />} />
 
-                <Route path="/cart" element={<Cart/>} /
+        <Route path="/cart" element={<Cart />} />
 
-                {/* loan schema */}
-                <Route path="/loan" element={<Loan />} />
-                <Route path="/loan/form1" element={<RequestForm1 />} />
-                <Route path="/loan/view_loan" element={<ViewLoans />} />
-                <Route path="/loan/form2" element={<RequestForm2 />} />
-                <Route path="/loan/view_loan2" element={<ViewLoans2 />} />
-                <Route path="/loan/doc" element={<DocumentSubmission />} />
+        {/* loan schema */}
+        <Route path="/loan" element={<Loan />} />
+        <Route path="/loan/form1" element={<RequestForm1 />} />
+        <Route path="/loan/view_loan" element={<ViewLoans />} />
+        <Route path="/loan/form2" element={<RequestForm2 />} />
+        <Route path="/loan/view_loan2" element={<ViewLoans2 />} />
+        <Route path="/loan/doc" element={<DocumentSubmission />} />
 
+        <Route path="/product/item/:id" element={<SingleItem />} />
+        <Route path="/category/add" element={<AddCategory />} />
+        <Route path="/category/view" element={<ViewCategory />} />
+        <Route path="/products/view/:id" element={<ViewProdcuts />} />
 
-                <Route path="/product/item/:id" element={<SingleItem/>}/>
-                <Route path="/category/add" element={<AddCategory/>}/>
-                <Route path="/category/view" element={<ViewCategory/>}/>
-                <Route path="/products/view" element={<ViewProdcuts/>}/>
+        <Route path="/products/add" element={<AddProducts />} />
+        <Route path="/requests" element={<ViewSeller />} />
+        <Route path="/all" element={<ViewUsers />} />
 
-                <Route path="/products/add" element={<AddProducts/>} />
-                <Route path="/requests" element={<ViewSeller/>} />
-                <Route path="/all" element={<ViewUsers/>} />
+        <Route path="/loan/form_1" element={<RequestForm1 />} />
 
-             
-                <Route path="/loan/form_1" element={<RequestForm1 />} />
-     
-                <Route path="/products/add" element={<AddProducts/>} />
-
-            </Routes>
-            <Footer/>
-        </Router>
-    );
+        <Route path="/products/add" element={<AddProducts />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
