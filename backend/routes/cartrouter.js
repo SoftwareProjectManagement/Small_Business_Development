fd50d233abb97c7715de5019ba990ca778590642
Ishan Nadeera <shanf753@gmail.com>
2022-10-10 23:16:48 +0530

    Update Cart Router

--- backend/routes/cartrouter.js
+++ backend/routes/cartrouter.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
-const {additem, viewCart, updateitem, deleteitem, viewOneCart} = require('../controllers/cartcontroller.js')
+const {additem, viewCart, updateitem, deleteitem, viewOneCart, viewCart1} = require('../controllers/cartcontroller.js')
 const userauth = require('../middleware/userauth');
 
 router.post('/add',userauth, additem);
 
 router.get('/:id', viewCart);
 
+router.get('/view1/:id', viewCart1);
+
 router.put('/update/:id', userauth, updateitem);
 
-router.delete('/delete/:id',userauth, deleteitem);
+router.delete('/delete/:id', deleteitem);
 
 router.get('/view/:id', viewOneCart);
 
