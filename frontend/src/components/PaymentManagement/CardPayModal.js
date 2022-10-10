f8af9a95116cca154d638adbd5dc4eb6192e757e
Ishan Nadeera <shanf753@gmail.com>
2022-10-10 23:18:07 +0530

    Created Add New Card Payment View

--- frontend/src/components/PaymentManagement/CardPayModal.js
+++ frontend/src/components/PaymentManagement/CardPayModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./CardPayModal.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
@@ -8,36 +8,63 @@ import { useNavigate } from "react-router-dom";
 import InputAdornment from "@material-ui/core/InputAdornment";
 
 function CardPay({ setOpenCardModal }) {
-  const [mobileNumber, setMobileNumber] = useState("");
-  const [pinNo, setPinNo] = useState("");
-  const [date, setDate] = useState("");
   const navigate = useNavigate();
-  const [creditCardNumber,setCreditCardNumber]= useState("");
+  const user = JSON.parse(localStorage.getItem("user"));
+  const userID = user._id;
+  let date = new Date().toLocaleDateString();
+  const amount = JSON.parse(localStorage.getItem("total"));
+  const [itemList, setItemList] = useState([]);
+  const cartItem = JSON.parse(localStorage.getItem("selectedItem"));
+
+  useEffect(() => {
+    const List = [...itemList];
+    cartItem.map((Cart) => getCartItems(Cart));
+
+    async function getCartItems(Cart) {
+      await axios
+        .get(`http://localhost:8070/cart/view1/${Cart}`)
+        .then((res) => {
+          let itemid = res.data.result.itemid;
+          let quantity = res.data.result.quantity;
+          List.push({ itemid, quantity });
+        })
+        .catch((error) => {
+          alert("Failed to fetch Items");
+        });
+    }
+    setItemList(List);
+  }, []);
 
   const config = {
     headers: {
-      "content-Type": "application/json",
+      "content-Type": "application/json"
     },
   };
 
   async function register(event) {
     event.preventDefault();
 
-    const newPayment = { mobileNumber, pinNo, date };
+    const newPayment = {
+      userID,
+      date,
+      amount,
+      itemList,
+    };
 
     try {
-      // await axios.post(
-      //   "http://localhost:8070/",
-      //   newPayment,
-      //   config
-      // );
+      await axios.post("http://localhost:8070/payment/add", newPayment,  config)
       Swal.fire({
         icon: "success",
         title: "Payment Successful!",
       }).then((result) => {
         if (result.isConfirmed) {
+          cartItem.map((Cart) => deleteItem(Cart));
+          localStorage.removeItem("selectedItem");
           navigate(`/`);
         } else {
+          cartItem.map((Cart) => deleteItem(Cart));
+          localStorage.removeItem("selectedItem");
+          navigate(`/`);
           navigate(`/`);
         }
       });
@@ -49,6 +76,13 @@ function CardPay({ setOpenCardModal }) {
       }
     }
   }
+
+  async function deleteItem(id){    
+    await axios.delete(`http://localhost:8070/cart/delete/${id}`, config).then(() => {
+    }).catch((error) => {
+        alert(`Failed to delete the item\n${error.message}`)
+    })
+}
 
   return (
     <div>
@@ -82,9 +116,6 @@ function CardPay({ setOpenCardModal }) {
                         placeholder="Credit Card Number"
                         required
                         fullWidth
-                        onChange={(event) => {
-                          setCreditCardNumber(event.target.value);
-                        }}
                         inputProps={{
                           style: { padding: 12 },
                           pattern:
@@ -131,7 +162,7 @@ function CardPay({ setOpenCardModal }) {
                         placeholder="Total Amount"
                         required
                         fullWidth
-                        value="500"
+                        value={amount}
                         readOnly
                         inputProps={{ style: { padding: 12 } }}
                         startAdornment={
