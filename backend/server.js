1e982faebd061b87b620c511bf5d19877db903c8
Hirosh Koshila <hiroshkoshila7@gmail.com>
2022-10-12 23:10:00 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,8 @@ const AdminRouter = require("./routes/adminrouter.js");
 const RequestRouter = require("./routes/requestrouter.js");
 const WorkshopRouter = require("./routes/workshoprouter.js");
 const CategoryRouter = require("./routes/categoryrouter.js");
-
 const ProductRouter = require("./routes/productrouter.js");
 const CartRouter = require("./routes/cartrouter.js");
-const LoanRouter = require("./routes/loanrouter");
-const ProductRouter = require("./routes/productrouter.js");
-const CartRouter = require("./routes/cartrouter");
 const LoanRouter = require("./routes/loanrouter");
 
 //getting the database url
